6c20b14f07a8b1d2658abd76d181fed3bea71288
Hustra03 <erik.paulinder@gmail.com>
2023-02-18 10:33:57 +0100

    Changes to be committed: 	modified:   display.c Changes not staged for commit: 	modified:   main.c  Untracked files: 	.vscode/ 	flappybird.h

--- display.c
+++ display.c
@@ -1,5 +1,6 @@
 #include <stdint.h>   /* Declarations of uint_32 and the like */
 #include <pic32mx.h>  /* Declarations of system-specific addresses etc */
+#include "flappybird.h"
 
 void display( int menu)
 {
